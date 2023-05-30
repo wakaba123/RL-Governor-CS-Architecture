0c5ff5ec04ff5d52fa1e4e309e3ff860b441ae10
hacksang <985438046@qq.com>
2023-05-30 16:19:41 +0800

    add variable to make compile.sh general

--- native/compile.sh
+++ native/compile.sh
@@ -1,3 +1,13 @@
-NDK=/home/blues/Desktop/android-ndk-r25c/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang++ 
+if [ $# != 2 ]; then
+  echo "USAGE: ./comple.sh server.cpp server"
+  exit -1;
+fi
+NDK_PATH=$(which ndk-build | xargs dirname)
+ANDROID_VERSION=$(adb shell getprop ro.build.version.sdk)
+NDK=$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android$ANDROID_VERSION-clang++ 
+
+echo "$NDK $1 fps.cpp -static-libstdc++ -o $2" 
 $NDK $1 fps.cpp -static-libstdc++ -o $2
+
+echo "adb push $2 /data/local/tmp"
 adb push $2 /data/local/tmp
