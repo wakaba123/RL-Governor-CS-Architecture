c2c7eda040bd3a05f0942a1c5677b25bbc4af7d0
hacksang <985438046@qq.com>
2023-06-07 10:47:27 +0800

    add ClusterNum and other arguments to increase portability

--- test_tools/modules/config.py
+++ test_tools/modules/config.py
@@ -1,3 +1,5 @@
+import os
+
 version = 9
 base =  10
 cpu_time = 2 * base 
@@ -5,8 +7,9 @@ mem_time = 4 * base
 io_time = 6 * base
 over_time = 8 * base
 
-test_file_path = '/home/blues/Desktop/networkTrans/test_tools/tests/'
+test_file_path = "/home/hacksang/Documents/study/博士/项目/横向项目/OPPO/RL_DVFS/RL-Governor-CS-Architecture/test_tools/tests/"
 
+#things = ['fps', 'little', 'big','lclock','bclock','mem']
 things = ['fps', 'little', 'big','lclock','bclock','mem']
 
 start_douyin = False
@@ -15,3 +18,6 @@ view = 'com.ss.android.ugc.aweme/com.ss.android.ugc.aweme.detail.ui.DetailActivi
 view = "com.smile.gifmaker/com.yxcorp.gifshow.HomeActivity#0"
 # view = 'tv.danmaku.bili/com.bilibili.video.videodetail.VideoDetailsActivity#0'
 # view = "com.ss.android.ugc.aweme/com.ss.android.ugc.aweme.splash.SplashActivity#0"
+
+ClusterNum = 3
+TargetFPS = 120
